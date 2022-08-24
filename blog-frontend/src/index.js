d4241e42d3317230c0c87d60dc58db191170e5e5
bombi <bombic3@naver.com>
2022-08-24 16:53:18 +0900

    BUILD : 로그인/회원가입 리덕스 생성

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,12 +3,21 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { legacy_createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './modules';
-import { legacy_createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
 
 const root = createRoot(document.getElementById('root'));
-const store = legacy_createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware)
+  )
+);
+
+sagaMiddleware.run(rootSaga);
 
 root.render(
   <Provider store={store}>
