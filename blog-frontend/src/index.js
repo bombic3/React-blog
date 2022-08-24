32081e7b5d782950429423482b258a52856e1751
bombi <bombic3@naver.com>
2022-08-24 13:49:30 +0900

    BUILD : 리덕스로 폼 상태관리

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,19 +1,19 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { legacy_createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
+import { legacy_createStore } from 'redux';
 
+const root = createRoot(document.getElementById('root'));
 const store = legacy_createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root'),
 );
