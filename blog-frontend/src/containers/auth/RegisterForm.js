48c88708532d047ae3c3b1f6130b959843e9acd1
bombi <bombic3@naver.com>
2022-08-25 14:52:03 +0900

    ADD : 회원인증 에러 처리 기능 구현

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -1,10 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
+import { useNavigate } from 'react-router-dom';
+
+/*
+- 다음 상황에 대한 에러를 모두 처리
+    - username, password, passwordConfirm 중 하나라도 비어 있을 때
+    - password 와 passwordConfirm 값이 일치하지 않을 때
+    - username이 중복될 때
+*/
 
 const RegisterForm = () => {
+  const [error, setError] = useState(null);
   const dispatch = useDispatch();
   const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
@@ -12,6 +21,8 @@ const RegisterForm = () => {
     authError: auth.authError,
     user: user.user
   }));
+  const navigate = useNavigate();
+
   // 인풋 변경 이벤트 핸들러
   const onChange = e => {
     const { value, name } = e.target;
@@ -28,8 +39,18 @@ const RegisterForm = () => {
   const onSubmit = e => {
     e.preventDefault();
     const { username, password, passwordConfirm } = form;
+    // 하나라도 비어 있다면
+    if ([username, password, passwordConfirm].includes('')) {
+      setError('빈 칸을 모두 입력하세요');
+      return;
+    }
+    // 비밀번호가 일치하지 않는다면
     if (password !== passwordConfirm) {
-      // TODO: 오류 처리
+      setError('비밀번호가 일치하지 않습니다.');
+      dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+      dispatch(
+        changeField({ form: 'register', key: 'passwordConfirm', value: '' }),
+      );
       return;
     }
     dispatch(register({ username, password }));
@@ -43,10 +64,19 @@ const RegisterForm = () => {
   // 회원가입 성공/실패 처리
   useEffect(() => {
     if (authError) {
-      console.log('오류발생');
-      console.log(authError);
+      // 계정명이 이미 존재할 때
+      if (authError.response.status === 409) {
+        setError('이미 존재하는 계정명입니다.');
+        return;
+      }
+      // 기타 이유
+      setError('회원가입 실패');
       return;
+      // console.log('오류발생');
+      // console.log(authError);
+      // return;
     }
+
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
@@ -58,10 +88,11 @@ const RegisterForm = () => {
   // user 값이 잘 설정되었는지 확인
   useEffect(() => {
     if (user) {
-      console.log('check API 성공');
-      console.log(user);
+      navigate('/'); // 홈 화면으로 이동
+      // console.log('check API 성공');
+      // console.log(user);
     }
-  }, [user]);
+  }, [navigate, user]);
 
   return (
     <AuthForm
@@ -69,6 +100,7 @@ const RegisterForm = () => {
       form={form}
       onChange={onChange}
       onSubmit={onSubmit}
+      error={error}
     />
   );
 };
