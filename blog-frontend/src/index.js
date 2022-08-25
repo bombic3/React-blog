5eb801b8e3cefae3d9f1451896d66d53c65ce750
bombi <bombic3@naver.com>
2022-08-25 13:47:16 +0900

    ADD : 회원가입/로그인 기능 구현

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -8,15 +8,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { rootSaga } from './modules';
 
-const root = createRoot(document.getElementById('root'));
 const sagaMiddleware = createSagaMiddleware();
 const store = legacy_createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(sagaMiddleware)
-  )
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
-
+const root = createRoot(document.getElementById('root'));
+    
 sagaMiddleware.run(rootSaga);
 
 root.render(
@@ -26,3 +24,32 @@ root.render(
     </BrowserRouter>
   </Provider>,
 );
+
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { legacy_createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import createSagaMiddleware from 'redux-saga';
+// import rootReducer, { rootSaga } from './modules';
+
+// const sagaMiddleware = createSagaMiddleware();
+// const store = legacy_createStore(
+//   rootReducer,
+//   composeWithDevTools(
+//     applyMiddleware(sagaMiddleware)
+//   )
+// );
+
+// sagaMiddleware.run(rootSaga);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root'),
+// );
